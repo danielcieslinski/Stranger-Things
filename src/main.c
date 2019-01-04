add0d38a6cd3d1d133854d2a1b4c84f4b0c10f27
danielcieslinski <daniel98c@gmail.com>
2019-01-04 04:45:18 +0100

    One step further to hell

--- src/main.c
+++ src/main.c
@@ -21,13 +21,37 @@ int capacity_of_waiting_room = 5;
 int n_of_customers = 5;
 
 //Time constants | time shall be given in seconds
-int max_time_of_customer_working = 20;
+int max_time_of_customer_working = 5;
 
+struct Utils {
+    int queue_msg, customer_msg;
+};
 
-void barber(int barber_id) {
+struct msgbuf {
+    long mtype; //typ komunikatu
+    int mvalue; //treść komunikatu
+};
+
+struct Utils utils_initializer() {
+
+    struct Utils utils;
+    utils.customer_msg=msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | IPC_NOWAIT | 0600); //
+
+    printf("utils initislized \n");
+
+    return utils;
+
+};
+
+void barber(int barber_id, struct Utils utils) {
 
     printf("New barber %d \n", barber_id);
-    while (true){}
+    struct msgbuf message;
+
+    while (true) {
+        while(msgrcv(utils.customer_msg, &message, sizeof(message.mvalue), 1, IPC_NOWAIT) == -1) {}
+        printf("Barber %d gives new haircut to %d customer \n",barber_id,  message.mvalue);
+    }
 
 }
 
@@ -39,15 +63,21 @@ void customer_works(int *wallet) {
 }
 
 
-void customer(int customer_id) {
+void customer(int customer_id, struct Utils utils) {
     srand(time(NULL) + customer_id);
 
     printf("New customer %d\n", customer_id);
     int wallet[] = {0, 0, 0};
 
+    //Message
+    struct msgbuf message;
+    message.mtype = 1;
+    message.mvalue = customer_id;
+
     while (true) {
         customer_works(wallet);
-        printf("Customer finished working %d \n", customer_id);
+        printf("Customer %d is ready and wants haircut \n", customer_id);
+        msgsnd(utils.customer_msg, &message, sizeof(message.mvalue), 0);
     }
 }
 
@@ -55,8 +85,8 @@ void customer(int customer_id) {
 int main() {
 
     int to_gen = n_of_barbers + n_of_customers;
+    struct Utils utils = utils_initializer();
 
-//    int queue_msg = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | IPC_NOWAIT | 0600); //kolejność zgłaszania się klientów
 //    int customer_msg = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | IPC_NOWAIT | 0600);
 //
 //    int sleeping_barbers_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0600); //wolni fryzjerzy
@@ -66,15 +96,15 @@ int main() {
 //    semctl(chair_sem, 0, SETVAL, N);
 //
 //    int customers_sem = semget(IPC_PRIVATE, C, IPC_CREAT | IPC_EXCL | 0600); //0 podczas obługi
-//    semctl(customers_sem, SETALL, 0);
+//    int queue_msg = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | IPC_NOWAIT | 0600); //Clients queue
 
     for (int i = 0; i < to_gen; i++) {
         if (fork() == 0) {
 
             if (i < n_of_barbers)
-                barber(i);
+                barber(i, utils);
 
-            else customer(i - n_of_barbers);
+            else customer(i - n_of_barbers, utils);
 
             break;
         }
